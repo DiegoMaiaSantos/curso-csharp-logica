955328aa257d7988f39e034a2334d38d04b10cf9
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-12-27 20:58:32 -0300

    Atualização da aula.

---
+++ saida-dados-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+double x = 10.35784;
+
+Console.WriteLine(x);
+Console.WriteLine(x.ToString("F2"));
+Console.WriteLine(x.ToString("F4"));
+
+Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+
+Console.Read();
