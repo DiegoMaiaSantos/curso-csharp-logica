380d4ecee3a6834cad6035aa7cb10851fd0249c4
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-12-27 21:25:09 -0300

    Aula finalizada.

--- saida-dados-csharp/Program.cs
+++ saida-dados-csharp/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System.Globalization;
 
 double x = 10.35784;
+int y = 32;
+string z = "Maria";
+char w = 'F';
 
+Console.WriteLine("Olá mundo!");
+Console.WriteLine("Bom dia!");
+Console.WriteLine("Até mais!");
+
+Console.WriteLine();
 Console.WriteLine(x);
 Console.WriteLine(x.ToString("F2"));
 Console.WriteLine(x.ToString("F4"));
-
 Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
 
+Console.WriteLine();
+Console.WriteLine("Resultado = " + x);
+Console.WriteLine("O valor do troco é " + x + " reais");
+Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+
+Console.WriteLine();
+Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
 
 Console.Read();
