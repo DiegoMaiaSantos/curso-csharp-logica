a3ccbe367aacb023a07a05e18da54d8d3bab3a10
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-12-28 09:23:58 -0300

    Parte 01 - Finalizada.

--- entrada-dados-csharp-1/Program.cs
+++ entrada-dados-csharp-1/Program.cs
@@ -5,8 +5,16 @@ string x = Console.ReadLine();
 string y = Console.ReadLine();
 string z = Console.ReadLine();
 
+string[] v = Console.ReadLine().Split(' ');
+string a = v[0];
+string b = v[1];
+string c = v[2];
+
 Console.WriteLine("Você digitou: ");
 Console.WriteLine(frase);
 Console.WriteLine(x);
 Console.WriteLine(y);
 Console.WriteLine(z);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
