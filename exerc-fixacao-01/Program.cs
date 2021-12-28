85dce485c0adf9bbb9fd76ec5b967bbd089c82b6
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-12-28 10:51:26 -0300

    Exercício de fixação

---
+++ exerc-fixacao-01/Program.cs
@@ -0,0 +1,13 @@
+﻿//Exercício de fixação
+
+//Fazer um programa para executar a seguinte 
+//unteração com o usuário, lendo os valores 
+//destacados em vermelho, e depois mostrar os dados na tela
+
+using System.Globalization
+
+string nome = string.Parse(Console.ReadLine());
+int n1 = int.Parse(Console.ReadLine());
+double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+string[] vet = Console.ReadLine().Split(' ');
