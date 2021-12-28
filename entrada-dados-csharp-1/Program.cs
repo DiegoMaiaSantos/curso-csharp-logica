1bf8529ce0e0f8404feb7c317236c78ff9227778
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-12-28 09:05:44 -0300

    Atualização do conteúdo da aula.

---
+++ entrada-dados-csharp-1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Entrada de dados C# - Parte 1
+
+string frase = Console.ReadLine();
+string x = Console.ReadLine();
+string y = Console.ReadLine();
+string z = Console.ReadLine();
+
+Console.WriteLine("Você digitou: ");
+Console.WriteLine(frase);
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
