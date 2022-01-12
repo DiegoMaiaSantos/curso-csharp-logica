e235786dda3a8a1b84db6ea90a7753c3fce07d0f
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-12 10:01:37 -0300

    Praticando exercícios da URI.

--- exerc-proposto-parte01/Program.cs
+++ exerc-proposto-parte01/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System; 
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int tempoHoras, velocidadeMedia;
+        double consumoVeiculo;
+
+        tempoHoras = int.Parse(Console.ReadLine());
+        velocidadeMedia = int.Parse(Console.ReadLine());  
+        consumoVeiculo = velocidadeMedia * tempoHoras / 12.0;
+
+        Console.WriteLine(consumoVeiculo.ToString("F3", CultureInfo.InvariantCulture));     
+    }
+
+}
