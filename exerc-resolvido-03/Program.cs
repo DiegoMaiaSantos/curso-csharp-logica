7ddbecdf92b36a8a1c623ec63b1796caa6c55422
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-13 21:42:08 -0300

    Resolução de exercícios 03.

---
+++ exerc-resolvido-03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            string nome1, nome2;
+            int idade1, idade2;
+            double mediaIdade;
+            
+            string[] vet;
+            vet = Console.ReadLine().Split(' ');
+            nome1 = vet[0];
+            idade1 = int.Parse(vet[1]);
+            
+            vet = Console.ReadLine().Split(' ');
+            nome2 = vet[0];
+            idade2 = int.Parse(vet[1]);
+
+            mediaIdade = (idade1 + idade2) / 2.0;
+
+            Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + mediaIdade.ToString("F1", CultureInfo.InvariantCulture) + " anos.");
+
+            Console.ReadLine();
+        }
+    }
+}
