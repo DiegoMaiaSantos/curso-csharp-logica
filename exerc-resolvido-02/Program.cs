31b7bc6aa61e04b9f6dc4e0e77ce21950495eb31
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-13 21:12:20 -0300

    Exercício resolvido 02.

---
+++ exerc-resolvido-02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+          double baseAltura, alturaRetangulo, area, perimetro, diagonal;
+
+          baseAltura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+          alturaRetangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+
+          area = baseAltura * alturaRetangulo;
+          perimetro = 2 * (baseAltura + alturaRetangulo);
+          diagonal = Math.Sqrt(Math.Pow(baseAltura, 2.0) + Math.Pow(alturaRetangulo, 2.0));
+
+          Console.WriteLine("Area = " + area.ToString("F4", CultureInfo.InvariantCulture));
+          Console.WriteLine("Perimetro = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+          Console.WriteLine("Diagnoal = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+          Console.ReadLine();  
+        }
+    }
+}
