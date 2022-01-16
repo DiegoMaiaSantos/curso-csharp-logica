72d58fe113359b9f96f67eae0a371ea722e76f64
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-16 16:13:52 -0300

    Exercícios.

---
+++ exerc-resolvido-04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int n, horas, resto, minutos, segundos;
+
+        n = int.Parse(Console.ReadLine());   
+
+        horas = n / 3600;
+        resto = n % 3600;
+
+        minutos = resto / 60;
+        segundos = resto % 60;
+
+        Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+        Console.ReadLine();  
+    }
+
+}
