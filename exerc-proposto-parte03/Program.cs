36de995764ecbf14d651ab16908420d0562e3ab9
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-17 20:43:55 -0300

    Praticando exercícios.
    Exercícios voltado para o aprendizado do (mod %).

--- exerc-proposto-parte03/Program.cs
+++ exerc-proposto-parte03/Program.cs
@@ -1,87 +1,82 @@
-﻿// Exercício Beecrowd 1018 (mod %)
+﻿// Exercício beecrowd 1021
 
 using System; 
+using System.Globalization;
 
 class URI {
 
     static void Main(string[] args) { 
 
-        int n, quociente, resto, nota;
+        double n;
+        int quociente, resto, nota, moeda;
 
-        n = int.Parse(Console.ReadLine());
-        Console.WriteLine(n);
+        n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        
+        resto = (int)(n * 100 + 0.5);   
 
-        resto = n;
+        Console.WriteLine("NOTAS:");    
 
         nota = 100;
-        quociente = resto / nota;
-        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
-        resto = resto % nota;
+        quociente = resto / (nota * 100);  
+        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ".00");
+        resto = resto % (nota * 100);
 
         nota = 50;
-        quociente = resto / nota;
-        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
-        resto = resto % nota;
+        quociente = resto / (nota * 100);  
+        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ".00");
+        resto = resto % (nota * 100);
 
         nota = 20;
-        quociente = resto / nota;
-        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
-        resto = resto % nota;
+        quociente = resto / (nota * 100);
+        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ".00");
+        resto = resto % (nota * 100);
 
         nota = 10;
-        quociente = resto / nota;
-        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00"); 
-        resto = resto % nota;
+        quociente = resto / (nota * 100);
+        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ".00");
+        resto = resto % (nota * 100);
 
         nota = 5;
-        quociente = resto / nota;
-        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
-        resto = resto % nota;
-        
+        quociente = resto / (nota * 100);
+        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ".00");
+        resto = resto % (nota * 100); 
+
         nota = 2;
-        quociente = resto / nota;
-        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ",00");
-        resto = resto % nota;   
+        quociente = resto / (nota * 100);
+        Console.WriteLine(quociente + " nota(s) de R$ " + nota + ".00");
+        resto = resto % (nota * 100); 
 
-        Console.WriteLine(resto + " nota(s) de R$ 1,00");    
-    }
+        Console.WriteLine("MOEDAS:");
 
-}
+        moeda = 100;
+        quociente = resto / moeda;
+        Console.WriteLine(quociente + " moeda(s) de R$ 1.00");
+        resto = resto % moeda; 
+
+        moeda = 50;
+        quociente = resto / moeda;
+        Console.WriteLine(quociente + " moeda(s) de R$ 0.50");
+        resto = resto % moeda;
+
+        moeda = 25;
+        quociente = resto / moeda;
+        Console.WriteLine(quociente + " moeda(s) de R$ 0.25");
+        resto = resto % moeda;
 
 
-// Exercício beecrowd 1020 (mod %)
+        moeda = 10;
+        quociente = resto / moeda;
+        Console.WriteLine(quociente + " moeda(s) de R$ 0.10");
+        resto = resto % moeda;
 
-using System; 
 
-class URI {
+        moeda = 5;
+        quociente = resto / moeda;
+        Console.WriteLine(quociente + " moeda(s) de R$ 0.05");
+        resto = resto % moeda;
 
-    static void Main(string[] args) { 
 
-        int n, anos, meses, dias, resto;
-
-        n = int.Parse(Console.ReadLine());
-
-        anos = n / 365;
-        resto = n % 365;
-        meses = resto / 30;
-        dias = resto % 30;
-
-        Console.WriteLine(anos + " anos(s)");
-        Console.WriteLine(meses + " mes(es)");
-        Console.WriteLine(dias + " dia(s)");        
-    }
-
-}
-
-// Exercício beecrowd 1021
-
-using System; 
-
-class URI {
-
-    static void Main(string[] args) { 
-
-        
+        Console.WriteLine(resto + " moeda(s) de R$ 0.01");
     }
 
 }
