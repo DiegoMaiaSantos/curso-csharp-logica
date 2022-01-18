942379edf861a5bdedf625de273cc0aa8683ea90
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-18 09:53:27 -0300

    Estruturas condicionais.
    Aula que explica as estruturas condicionais.
    
    if (condição) {
       comando;
    }
    else {
            comando;
    }
    
    Ou:
    
    if (condição 1) {
       comando 1;
    }
    else if (condição 2) {
         comando 2;
    }
    else {
         comando 3;
    }

---
+++ estrutura-condicional/Program.cs
@@ -0,0 +1,75 @@
+﻿// Simples:
+
+using System;
+
+namespace testes_ec {
+    class Program {
+        static void Main(string[] args) {
+
+            int x = 5;
+            
+            Console.WriteLine("Bom dia");
+
+            if (x < 0) {
+                Console.WriteLine("Boa tarde");
+            }
+
+            Console.WriteLine("Boa noite");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+//Composta:
+
+using System;
+
+namespace testes_ec {
+    class Program {
+        static void Main(string[] args) {
+
+            int horas;
+
+            Console.Write("Quantas horas? ");
+            horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+            else {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Encadeamento de estruturas condicionais:
+
+using System;
+
+namespace testes_ec {
+    class Program {
+        static void Main(string[] args) {
+
+            int horas;
+
+            Console.Write("Quantas horas? ");
+            horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (horas < 18) { 
+                Console.WriteLine("Boa tarde!");                   
+            }
+            else {
+                Console.WriteLine("Boa noite!");
+            }            
+
+            Console.ReadLine();
+        }
+    }
+}
