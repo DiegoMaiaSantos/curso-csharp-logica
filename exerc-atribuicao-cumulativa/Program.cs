5ea606ac1a3dcb0ae0839e9dde796d340174ebca
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-19 00:07:02 -0300

    Operador de atribuição cumulativa.
    Atribuição cumulativa:
    
    a += b;
    a -= b;
    a *= b;
    a /= b;
    a %= b;

---
+++ exerc-atribuicao-cumulativa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100) {
+                conta += (minutos - 100) * 2.0;
+            }
+
+            Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
