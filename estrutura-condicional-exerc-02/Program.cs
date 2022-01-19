f44d6c7ed5ffea53834048c02d7fcf48f96c1b57
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-18 23:46:32 -0300

    Conteúdo de estruturas condicional - Exercícios.

---
+++ estrutura-condicional-exerc-02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program {
+    static void Main(string[] args) {
+
+        int x, y, z;
+
+        string[] valores = Console.ReadLine().Split(' ');
+        x = int.Parse(valores[0]);
+        y = int.Parse(valores[1]);
+        z = int.Parse(valores[2]);
+
+        if (x < y && x < z) {
+            Console.WriteLine("Menor = " + x);
+        }   
+        else if (y < z) {
+            Console.WriteLine("Menor = " + y);
+        } 
+        else {
+            Console.WriteLine("Menor = " + z);            
+        }     
+
+        Console.ReadLine();       
+    }
+}
