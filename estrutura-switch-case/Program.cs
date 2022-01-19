ff30fd8cecbbedd004788e6a7dff0942b799dee9
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-19 00:36:03 -0300

    Estrutura de switch-case.

---
+++ estrutura-switch-case/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+            string dia;
+
+            switch (x)
+            {
+                case 1:
+                    dia = "domingo";
+                    break;
+                case 2:
+                    dia = "segunda";
+                    break;
+                case 3:
+                    dia = "terça";
+                    break;
+                case 4:
+                    dia = "quarta";
+                    break;
+                case 5:
+                    dia = "quinta";
+                    break;
+                case 6:
+                    dia = "sexta";
+                    break;
+                case 7:
+                    dia = "sábado";
+                    break;
+                default:
+                    dia = "valor inválido";
+                    break;
+            }
+            if (x <= 7) {
+                Console.WriteLine("Dia da semana: " + dia + "-feira.");
+            }
+            else {
+                Console.WriteLine("Valor inválido!");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
