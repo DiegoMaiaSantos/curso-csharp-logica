35db99cac7ffc7c4f5d8a30e412de9d8210df857
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-19 22:57:11 -0300

    Exercícios da URI.

---
+++ exerc-proposto-parte04/Program.cs
@@ -0,0 +1,12 @@
+﻿//Exercício Beecrowd 1046:
+
+using System;
+using System.Globalization;
+
+class Program {
+
+    static void Main(string[] args) {          
+
+        Console.ReadLine();    
+    }
+}
