2167f69d3220d36440f0634d21bd9d22bdf083db
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-22 20:15:27 -0300

    Introdução a seção 5.
    Estruturas de repetição:
    
    Primeira estrutura - while.

---
+++ estrutura-repetitiva-while/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+
+        int x, soma;
+
+        x = int.Parse(Console.ReadLine());
+        soma = 0;
+        while (x != 0) {
+            soma += x;
+            x = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(soma);
+
+        Console.ReadLine();
+    }
+}
