e792e00f129ed5e6b4473340fe155f2947808894
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-23 16:34:31 -0300

    Estrutura repetitiva while.
    Exercício resolvido de estrutura repetitiva while.
    
    while (condição) {
            comando 01;
            comando 02;
    }

---
+++ exerc-resolvido-while-01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int x, y;
+
+        string[] valores = Console.ReadLine().Split(' ');
+        x = int.Parse(valores[0]);
+        y = int.Parse(valores[1]);
+
+        while (x != y) {
+            if (x < y) {
+                Console.WriteLine("Crescente");
+            }    
+            else {
+                Console.WriteLine("Decrescente");
+            } 
+            valores = Console.ReadLine().Split(' ');
+            x = int.Parse(valores[0]);
+            y = int.Parse(valores[1]);       
+        }
+    }
+}
