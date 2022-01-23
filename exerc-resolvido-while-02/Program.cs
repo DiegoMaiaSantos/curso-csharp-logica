a96cca6ef0fc94ae3ffbebd259802b914d3cdfcc
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-23 17:13:54 -0300

    Segundo exercício resolvido - while.

---
+++ exerc-resolvido-while-02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+class Program {
+    static void Main(string[] args) {
+
+        double idade, soma, media;
+        int contador;
+
+        idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);  
+
+        soma = 0.0;
+        contador = 0;
+        while (idade >= 0) {
+            soma += idade;
+            contador += 1;
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+        }
+        if (contador == 0) {
+            Console.WriteLine("Impossivel calcular");
+        }
+        else {
+            media = soma / contador;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        } 
+
+        Console.ReadLine();        
+    }
+}
