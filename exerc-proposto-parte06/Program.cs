3eed19ff2d6b5bd76f272d4a0be43410aa099b06
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-26 23:08:20 -0300

    Exercício 1118 - Beecrowd.

--- exerc-proposto-parte06/Program.cs
+++ exerc-proposto-parte06/Program.cs
@@ -178,3 +178,43 @@ class Program {
         Console.ReadLine();
     }
 }
+
+// Exercícios Beecrowd 1118:
+
+using System;
+using System.Globalization;
+class Program {
+    static void Main(string[] args) {
+
+        double nota1, nota2, media, novoCalc;
+
+        novoCalc = 1;
+
+        while (novoCalc == 1) {
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (nota1 < 0.0 || nota1 > 10.0) {
+                Console.WriteLine("nota invalida");
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (nota2 < 0.0 || nota2 > 10.0) {
+                Console.WriteLine("nota invalida");
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            media = (nota1 + nota2) / 2.0;
+            Console.WriteLine("media = " + media.ToString("F2"), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("novo calculo (1-sim 2-nao)");
+            novoCalc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (novoCalc != 1 && novoCalc != 2) {
+                Console.WriteLine("novo calculo (1-sim 2-nao)");
+                novoCalc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+ 
+            }
+        }
+        
+        Console.ReadLine();
+    }
+}
