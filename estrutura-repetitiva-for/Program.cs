569ab87cba2842afdee3ca44641d9827c2e3c5b2
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-29 20:57:05 -0300

    Conteúdo de introdução estrutura "for"
    Dando início ao conteúdo da estrutura repetitiva "for".
    
    Exemplo da sintaxe/regra:
    
    for (início; condição; incremento) {
         Comando 1
         Comando 2
    }
    
    É uma estrutura de controle que repete um bloco de comandos para um certo intervalo de valores.

---
+++ estrutura-repetitiva-for/Program.cs
@@ -0,0 +1,56 @@
+﻿//Estrutura repetitiva "for":
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int n, x, i, soma;
+
+        n = int.Parse(Console.ReadLine());
+        
+        soma = 0;
+        // início - condição - incremento:   
+        for (i = 1; i <= n; i ++) {
+            x = int.Parse(Console.ReadLine());
+            soma += x;
+        }
+
+        Console.WriteLine(soma);
+        Console.ReadLine();
+    }
+}
+
+// Contagem decrescente de 0 até 10:
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int i;
+
+        for (i = 0; i < 11; i ++) {
+            Console.WriteLine("Valor de i: " + i);
+        }
+
+        Console.ReadLine();
+    }
+}
+
+//Contagem regressiva de 10 até 0:
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int i;
+
+        for (i = 10; i >= 0; i --) {
+            Console.WriteLine("Valor de i: " + i);
+        }
+
+        Console.ReadLine();
+    }
+}
