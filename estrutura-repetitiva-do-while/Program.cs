437beb1f5372aaf7ed5e831d437299d318328cb0
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-30 11:12:44 -0300

    Estrutura repetitiva do-while
    Aula sobre a estrutura repetitiva do-while.
    
    do {
          Comando 1
          Comando 2
    } while (Condição);

---
+++ estrutura-repetitiva-do-while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+class Program {
+    static void Main(string[] args) {
+
+        double c, f;
+        char repetir;
+
+        do {
+            Console.WriteLine("Digite a temperatura em Celsius: ");
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Formula de celsius para fahrenheit:
+            f = 9.0 * c / 5.0 + 32.0;
+            Console.WriteLine("Equivalente em Fahrenheit: " + f.ToString("F1", CultureInfo.InvariantCulture));
+            Console.Write("Deseja repetir (s/n)? ");
+            repetir = char.Parse(Console.ReadLine());
+        } while (repetir == 's');
+
+        Console.ReadLine();
+    }
+}
