ce6847c4fd335b75630d802969788e9cafec57dc
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-01-29 23:11:18 -0300

    Exercício resolvido "for"
    Exercício de estrutura repetitiva "for" para ajudar na fixação do conteúdo estudado.

---
+++ exerc-resolvido-for-01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int n, i, resultado;
+
+        n = int.Parse(Console.ReadLine());
+        
+        for (i = 1; i <= 10; i ++) {
+            resultado = i * n;
+            Console.WriteLine(i + " x " + n + " = " + resultado);
+        }
+
+        Console.ReadLine();
+    }
+}
