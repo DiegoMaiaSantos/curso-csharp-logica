ffd7c49c36e9feb43df650d42af832eca3febe17
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-06 18:51:27 -0300

    Novo conteúdo "Vetores"
    Iniciando um novo conteúdo "Vetores".
    
    Declaração => int[ ] a;
                             double[ ] b;
                             string[ ] c;
    
    Instanciação => a = new int[10];
                               b = new double[5];
                               c = new string[8];
    
    Para acessar os elementos de um vetor:
    
    a[3] = 10;   b[5] = 6;   c[1] = "Maria";

---
+++ vetores/Program.cs
@@ -0,0 +1,27 @@
+﻿// Novo conteúdo: Vetores.
+
+using System;
+using System.Globalization;
+
+class Program {
+    static void Main(string[] args) {
+
+        int n, i;
+        double[] vet; // Para definir o tipo de vetor.
+
+        n = int.Parse(Console.ReadLine());
+        vet = new double[n]; // Para instanciar o vetor.
+
+        // Esse for vai receber os valores e armazenar.
+        for (i = 0; i < n; i ++) {
+            vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+        
+        // Esse for vai mostrar na tela os valores.
+        for (i = 0; i < n; i ++) {
+            Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+        }
+
+        Console.ReadLine();
+    }
+}
