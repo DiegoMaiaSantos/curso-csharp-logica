18b689e7eb67e52fba7ca5940cdd51f5bf53f735
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-07 16:40:39 -0300

    Exercício resolvido de vetores 01

---
+++ exerc-resolvido-vetores-01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int n, i;
+        int[] vetores;
+
+        n = int.Parse(Console.ReadLine());
+        vetores = new int[n];
+
+        string[] valores = Console.ReadLine().Split(' ');
+        for (i = 0; i < n; i ++) {
+            vetores[i] = int.Parse(valores[i]);
+        }
+
+        for (i = 0; i < n; i ++) {
+            if (vetores[i] < 0) {
+                Console.WriteLine(vetores[i]);
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
