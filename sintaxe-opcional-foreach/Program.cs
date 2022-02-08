833bf8e69963725295bde9f5cf47dd66f030b0da
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-08 08:53:21 -0300

    Sintaxe opcional "foreach"
    Exemplo:
    
    foreach (string elemento in vet) {
               Console.WriteLine(elemento);
    }

---
+++ sintaxe-opcional-foreach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int n;
+        n = int.Parse(Console.ReadLine());
+        string[] vet = new string[n];
+
+        // Entrada de dados ===============================================
+        for (int i = 0; i < n; i++) {
+            vet[i] = Console.ReadLine();
+        }
+
+        // Saída de dados =================================================
+        Console.WriteLine("Nomes lidos:");
+        foreach (string x in vet) { // Muda de acordo com o tipo: string, int, double e etc.
+            Console.WriteLine(x);
+        }
+
+        Console.ReadLine();
+    }
+}
