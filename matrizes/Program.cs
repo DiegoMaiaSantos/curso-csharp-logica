eb4b02ac483eabcff4e5d8adcfbb33f762a25fbd
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-09 09:20:46 -0300

    Novo conteúdo "matrizes"
    - Declaração: double[,] a;
    
    - Instanciação: a = new double[3, 4];
    
    - Acesso: a[1, 2] = 10;

---
+++ matrizes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int m, n;
+        int[,] a;
+
+        string[] vet = Console.ReadLine().Split();
+        m = int.Parse(vet[0]);
+        n = int.Parse(vet[1]);
+        a = new int[m, n];
+
+        for (int i = 0; i < m; i++) {
+            string[] s = Console.ReadLine().Split(' ');
+            for (int j = 0; j < n; j++) {
+                a[i, j] = int.Parse(s[j]);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                Console.Write(a[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
