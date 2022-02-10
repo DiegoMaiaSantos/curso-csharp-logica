e89eeb9389974b3e800d6f73c08c4d498eaa48fa
DiegoMaiaSantos <diegom.santos03@gmail.com>
2022-02-09 23:25:03 -0300

    Exercícios de matrizes.

---
+++ exerc-resolvido-matrizes-01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int n;
+        int[,] a;
+
+        n = int.Parse(Console.ReadLine());
+        a = new int[n, n];
+
+        for (int i = 0; i < n; i++) {
+            string[] s = Console.ReadLine().Split();
+            for (int j = 0; j < n; j++) {
+                a[i, j] = int.Parse(s[j]);                
+            }
+        }
+
+        Console.WriteLine("DIAGONAL PRINCIPAL:");
+        for (int i = 0; i < n; i++) {
+            Console.Write(a[i, i] + " ");
+        }
+        Console.WriteLine();
+
+        int cont = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i, j] < 0) {
+                    cont++;
+                }
+            }
+        }
+        Console.WriteLine("QUANTIDADE DE NEGATIVOS = " + cont);
+        Console.ReadLine();
+    }
+}
---
+++ exerc-resolvido-matrizes-02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        int m, n;
+        int[,] a;
+
+        string[] s = Console.ReadLine().Split(' ');
+        m = int.Parse(s[0]);
+        n = int.Parse(s[1]);
+        a = new int[m, n];
+        
+        for (int i = 0; i < m; i++) {
+            s = Console.ReadLine().Split(' ');
+            for (int j = 0; j < n; j++) {
+                a[i, j] = int.Parse(s[j]);
+            }
+        }
+
+        int[] vet = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            int soma = 0;
+            for (int j = 0; j < n; j++) {
+                soma += a[i, j];
+            }
+            vet[i] = soma;
+        }
+
+        for (int i = 0; i < m; i++) {
+            Console.WriteLine(vet[i]);
+        }
+
+        Console.ReadLine();
+    }
+}
